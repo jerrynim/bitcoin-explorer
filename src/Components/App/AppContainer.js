90dc18da7d485a4801c99e2ff346bfde9e5519fb
jerrynim <tjerry3@naver.com>
2019-01-28 17:44:04 +0900

    typography

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import { createGlobalStyle } from "styled-components";
 import AppPresenter from "./AppPresenter";
 import reset from "styled-reset";
+import typography from "../../typography";
 
 const baseStyles = () => createGlobalStyle`
     ${reset}
+    ${typography}
 `;
 
 class AppContainer extends Component {
